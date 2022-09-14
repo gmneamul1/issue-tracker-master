d18e1189685d563be0d931e58a48873c0130cb64
gmneamul1 <gmneamul1@gmail.com>
2022-09-14 15:54:42 +0600

    finish

--- main.js
+++ main.js
@@ -45,13 +45,16 @@ const fetchIssues = () => {
   for (var i = 0; i < issues.length; i++) {
     const {id, description, severity, assignedTo, status} = issues[i];
 
+    let ifDisabled = 'open';
+    status === 'Closed'? ifDisabled ='disabled' : ifDisabled = '';
+
     issuesList.innerHTML +=   `<div class="well">
                               <h6>Issue ID: ${id} </h6>
                               <p><span class="label label-info"> ${status} </span></p>
                               <h3> ${description} </h3>
                               <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="setStatusClosed('${id}')" class="btn btn-warning">Close</a>
+                              <a href="#" onclick="closeIssue('${id}')" class="btn btn-warning ${ifDisabled}">Close</a>
                               <a href="#" onclick="deleteIssue('${id}')" class="btn btn-danger">Delete</a>
                               </div>`;
   }
